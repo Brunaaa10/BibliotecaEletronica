81fb641be910c66ceb81c80194912becac4bd6a2
brunaaa10 <brunaleticiadiniz1@gmail.com>
2023-06-27 21:19:51 -0300

    QR code

--- Screens/Transaction.js
+++ Screens/Transaction.js
@@ -1,26 +1,79 @@
 import React from "react";
-import {View, Text, StyleSheet} from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import * as Permissions from "expo-permissions";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
-export default class Transaction extends React.Component{
-    render(){
-        return(
+export default class Transaction extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            domState: "normal",
+            scanned: false,
+            hasCameraPermissions: null,
+            scannedData: "",
+        }
+    }
+    getCameraPermissions = async (domState) => {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA)
+        this.setState({
+            hasCameraPermissions: status == 'granted',
+            domState: domState,
+            scanned: false,
+        })
+    }
+    handleBarcodeScanned = async ({ type, data }) => {
+        this.setState({
+            scannedData: data,
+            domState: "normal",
+            scanned: true,
+        })
+    }
+    render() {
+        const { domState, hasCameraPermissions, scanned, scannedData } = this.state
+        if (domState == 'scanner') {
+            return (
+                <BarCodeScanner
+                    onBarCodeScanned={scanned ? undefined : this.handleBarcodeScanned}
+                    style={StyleSheet.absoluteFillObject}
+                />
+            )
+        }
+        return (
             <View style={styles.container}>
-                <Text style = {styles.text}>Tela de transação</Text>
+                <Text style={styles.buttonText}>
+                    {hasCameraPermissions ? scannedData : "Dê permissão para acessar a câmera!!"}
+                </Text>
+                <TouchableOpacity onPress={() => this.getCameraPermissions("scanner")} style={styles.button}>
+                    <Text>Digitalizar</Text>
+                </TouchableOpacity>
 
             </View>
         )
     }
 }
 
-const styles= StyleSheet.create({
-    container:{
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: "#5653D4",
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#5653D4"
     },
-    text:{
-        color: "white",
-        fontSize: 30,
+    text: {
+        color: "#ffff",
+        fontSize: 15
+    },
+    button: {
+        width: "43%",
+        height: 55,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F48D20",
+        borderRadius: 15
+    },
+    buttonText: {
+        fontSize: 15,
+        color: "#FFFFFF"
     }
-})
+
+});
