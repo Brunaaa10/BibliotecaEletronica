e9b48c97a7081c96a3f1d831d2d505bc6191197e
brunaaa10 <brunaleticiadiniz1@gmail.com>
2023-08-04 18:59:41 -0300

    Login e Search

--- App.js
+++ App.js
@@ -1,13 +1,57 @@
 import React from 'react';
 import TabNavigator from './Navigation/TabNavigator';
 import { NavigationContainer } from '@react-navigation/native';
+import { Rajdhani_600SemiBold } from "@expo-google-fonts/rajdhani";
+import * as Font from "expo-font";
+import LoginScreen from './Screens/Login';
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <TabNavigator/>
+import { createSwitchNavigator, createAppContainer } from "react-navigation";
 
-    </NavigationContainer>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      fontLoaded: false
+    };
+  }
+
+  async loadFonts() {
+    await Font.loadAsync({
+      Rajdhani_600SemiBold: Rajdhani_600SemiBold
+    });
+    this.setState({ fontLoaded: true });
+  }
+
+  componentDidMount() {
+    this.loadFonts();
+  }
+
+  render() {
+    const { fontLoaded } = this.state;
+    if (fontLoaded) {
+      return (
+        <NavigationContainer>
+            <AppContainer />
+        </NavigationContainer>
+      ) 
+    }
+    return null;
+  }
 }
+
+const AppSwitchNavigator = createSwitchNavigator(
+  {
+    Login: {
+      screen: LoginScreen
+    },
+    BottomTab: {
+      screen: TabNavigator
+    }
+  },
+  {
+    initialRouteName: "Login"
+  }
+);
+
+const AppContainer = createAppContainer(AppSwitchNavigator);
 
